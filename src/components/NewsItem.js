0c23b71bd7c9ff06e7e7c9ede37fea6c96232eb8
Sanket Mudholkar <93979254+sanket0708@users.noreply.github.com>
2022-08-19 01:43:53 +0530

    Components structure

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react'
+import { getByTitle } from "@testing-library/react";
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let { title, description } = this.props;
     return (
-      <div>This is NewsItem component</div>
-    )
+      <div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="https:////m.files.bbci.co.uk/modules/bbc-morph-sport-seo-meta/1.22.0/images/bbc-sport-logo.png"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/newsdetail" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
