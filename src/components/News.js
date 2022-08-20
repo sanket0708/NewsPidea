df71594aa35ff3d236bdaf3844fb01fee2299b7c
Sanket Mudholkar <93979254+sanket0708@users.noreply.github.com>
2022-08-21 01:28:08 +0530

    Creating next and previous buttons

--- src/components/News.js
+++ src/components/News.js
@@ -8,18 +8,55 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
+      page: 1,
     };
   }
 
   async componentDidMount() {
     //after render this will run
     let url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=5ca12917ccaa444bb35822fff6a5b438";
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=5ca12917ccaa444bb35822fff6a5b438&page=1pageSize=20";
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({ articles: parsedData.articles });
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
   }
+
+  handlePrevClick = async () => {
+    console.log("previous");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=5ca12917ccaa444bb35822fff6a5b438&page=${
+      this.state.page - 1
+    }&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+
+  handleNextClick = async () => {
+    console.log("next");
+    if (this.state.page + 1 > Math.ceil(this.totalResults / 18)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=5ca12917ccaa444bb35822fff6a5b438&page=${
+        this.state.page + 1
+      }&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
 
   render() {
     return (
@@ -39,6 +76,24 @@ export class News extends Component {
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            {" "}
+            &larr;Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
